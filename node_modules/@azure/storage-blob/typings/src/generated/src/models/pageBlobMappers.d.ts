d727b851b0c3908f5cd8b93d03424207b8433baa
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2021-01-28 15:28:12 -0800

    Accept remote build options

---
+++ node_modules/@azure/storage-blob/typings/src/generated/src/models/pageBlobMappers.d.ts
@@ -0,0 +1,2 @@
+export { ClearRange, PageBlobClearPagesHeaders, PageBlobCopyIncrementalHeaders, PageBlobCreateHeaders, PageBlobGetPageRangesDiffHeaders, PageBlobGetPageRangesHeaders, PageBlobResizeHeaders, PageBlobUpdateSequenceNumberHeaders, PageBlobUploadPagesFromURLHeaders, PageBlobUploadPagesHeaders, PageList, PageRange, StorageError } from "../models/mappers";
+//# sourceMappingURL=pageBlobMappers.d.ts.map

bbe78d154a65d4283bf0d0af6002e1e4275d2e25
Chandni Patel <chandnipatel@microsoft.com>
2022-08-15 13:38:58 -0500

    validation for windows only

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -5,6 +5,7 @@ import { Logger } from './logger';
 import { WebRequestError } from '../exceptions';
 import { IActionContext } from '../interfaces/IActionContext';
 import { AuthenticationType } from '../constants/authentication_type';
+import { RuntimeStackConstant } from '../constants/runtime_stack';
 
 const fs = require("fs");
 
@@ -96,15 +97,17 @@ export class Client {
             let password = "";
             let uri = "";
 
-            if (context.authenticationType === AuthenticationType.Scm) {
-                Logger.Info("Validating deployment package for functions app before Zip Deploy (SCM)");
-                if (context.scmCredentials){
-                    username = context.scmCredentials.username;
-                    password = context.scmCredentials.password;
-                    uri = context.scmCredentials.uri;
-                }
-            }
-            else if (context.authenticationType === AuthenticationType.Rbac) {
+            // if (context.authenticationType === AuthenticationType.Scm) {
+            //     Logger.Info("Validating deployment package for functions app before Zip Deploy (SCM)");
+            //     if (context.scmCredentials){
+            //         username = context.scmCredentials.username;
+            //         password = context.scmCredentials.password;
+            //         uri = context.scmCredentials.uri;
+            //     }
+            // }
+            
+            if (context.authenticationType === AuthenticationType.Rbac &&
+                context.os === RuntimeStackConstant.Windows) {
                 Logger.Info("Validating deployment package for functions app before Zip Deploy (RBAC)");
                 var publishingCredentials = await context.appService.getPublishingCredentials();
                 if (publishingCredentials.properties["scmUri"]) {
@@ -112,34 +115,34 @@ export class Client {
                     password = publishingCredentials.properties["publishingPassword"];
                     uri = publishingCredentials.properties["scmUri"];
                 }                
-            }
 
-            const base64Cred: string = Buffer.from(`${username}:${password}`).toString('base64');
-            let request: WebRequest = {
-                method: 'POST',
-                uri: `${uri}/api/zipdeploy/validate`,
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Basic ${base64Cred}`
-                },
-                body: fs.createReadStream(webPackage)
-            };
+                const base64Cred: string = Buffer.from(`${username}:${password}`).toString('base64');
+                let request: WebRequest = {
+                    method: 'POST',
+                    uri: `${uri}/api/zipdeploy/validate`,
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Basic ${base64Cred}`
+                    },
+                    body: fs.createReadStream(webPackage)
+                };
 
-            let response = await Client.webClient.sendRequest(request);
-            if(response.statusCode == 200) {
-                Logger.Info(`##[debug]Validation passed response: ${JSON.stringify(response)}`);
-                if (response.body && response.body.result){
-                    Logger.Warn(JSON.stringify(response.body.result));          
+                let response = await Client.webClient.sendRequest(request);
+                if(response.statusCode == 200) {
+                    Logger.Info(`##[debug]Validation passed response: ${JSON.stringify(response)}`);
+                    if (response.body && response.body.result){
+                        Logger.Warn(JSON.stringify(response.body.result));          
+                    }
+                    return null;
                 }
-                return null;
-            }
-            else if(response.statusCode == 400) {         
-                Logger.Info(`##[debug]Validation failed response: ${JSON.stringify(response)}`);      
-                throw response;
-            }
-            else {
-                Logger.Info(`##[debug]Skipping validation with status: ${response.statusCode}`);
-                return null;
+                else if(response.statusCode == 400) {         
+                    Logger.Info(`##[debug]Validation failed response: ${JSON.stringify(response)}`);      
+                    throw response;
+                }
+                else {
+                    Logger.Info(`##[debug]Skipping validation with status: ${response.statusCode}`);
+                    return null;
+                }
             }
         }
         catch(error) {

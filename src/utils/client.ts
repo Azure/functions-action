36dad8a1a9777a0b86b8dc3178fd540a07ba8525
Chandni Patel <88407036+patelchandni@users.noreply.github.com>
2022-08-15 09:44:28 -0500

    Patelchandni/validation (#148)
    * Calling validation API before ZipDeploy
    * Added 30 seconds wait before calling sync trigger
    * Fixed typo: Github -> GitHub

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -3,6 +3,10 @@ import { WebClient, WebRequest, WebRequestOptions, WebResponse } from 'azure-act
 import { IScmCredentials } from '../interfaces/IScmCredentials';
 import { Logger } from './logger';
 import { WebRequestError } from '../exceptions';
+import { IActionContext } from '../interfaces/IActionContext';
+import { AuthenticationType } from '../constants/authentication_type';
+
+const fs = require("fs");
 
 export class Client {
     public static webClient = new WebClient();
@@ -82,6 +86,69 @@ export class Client {
                 throw new WebRequestError(scm.uri, 'DELETE', 'Failed to delete app setting via kudu', expt);
             } else {
                 Logger.Warn(`Failed to perform DELETE ${scm.uri}`);
+            }
+        }
+    }
+
+    public static async validateZipDeploy(context: IActionContext, webPackage: string): Promise<any> {
+        try {
+            let username = "";
+            let password = "";
+            let uri = "";
+
+            if (context.authenticationType === AuthenticationType.Scm) {
+                Logger.Info("Validating deployment package for functions app before Zip Deploy (SCM)");
+                if (context.scmCredentials){
+                    username = context.scmCredentials.username;
+                    password = context.scmCredentials.password;
+                    uri = context.scmCredentials.uri;
+                }
+            }
+            else if (context.authenticationType === AuthenticationType.Rbac) {
+                Logger.Info("Validating deployment package for functions app before Zip Deploy (RBAC)");
+                var publishingCredentials = await context.appService.getPublishingCredentials();
+                if (publishingCredentials.properties["scmUri"]) {
+                    username = publishingCredentials.properties["publishingUserName"];
+                    password = publishingCredentials.properties["publishingPassword"];
+                    uri = publishingCredentials.properties["scmUri"];
+                }                
+            }
+
+            const base64Cred: string = Buffer.from(`${username}:${password}`).toString('base64');
+            let request: WebRequest = {
+                method: 'POST',
+                uri: `${uri}/api/zipdeploy/validate`,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Basic ${base64Cred}`
+                },
+                body: fs.createReadStream(webPackage)
+            };
+
+            let response = await Client.webClient.sendRequest(request);
+            if(response.statusCode == 200) {
+                Logger.Info(`##[debug]Validation passed response: ${JSON.stringify(response)}`);
+                if (response.body && response.body.result){
+                    Logger.Warn(JSON.stringify(response.body.result));          
+                }
+                return null;
+            }
+            else if(response.statusCode == 400) {         
+                Logger.Info(`##[debug]Validation failed response: ${JSON.stringify(response)}`);      
+                throw response;
+            }
+            else {
+                Logger.Info(`##[debug]Skipping validation with status: ${response.statusCode}`);
+                return null;
+            }
+        }
+        catch(error) {
+            if (error && error.body && error.body.result && typeof error.body.result.valueOf() == 'string' && error.body.result.includes('ZipDeploy Validation ERROR')) {
+                throw new Error(JSON.stringify(error.body.result));
+            }           
+            else {
+                Logger.Info(`##[debug]Skipping validation with error: ${JSON.stringify(error)}`);
+                return null;
             }
         }
     }

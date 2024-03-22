cb6534eae689f9be3195e0a617216f5fdfa8795a
longmauriceproj <longmaurice255@gmail.com>
2024-03-22 08:31:54 -0700

    Bump dependencies to the latest versions

--- src/appservice-rest/Kudu/KuduServiceClient.ts
+++ src/appservice-rest/Kudu/KuduServiceClient.ts
@@ -1,8 +1,13 @@
-import util = require('util');
+import util = require("util");
 
-import { WebClient, WebRequest, WebRequestOptions, WebResponse } from 'azure-actions-webclient/WebClient';
+import {
+    WebClient,
+    WebRequest,
+    WebRequestOptions,
+    WebResponse,
+} from "azure-actions-webclient/WebClient";
 
-import core = require('@actions/core');
+import * as core from "@actions/core";
 
 export class KuduServiceClient {
     private _scmUri;
@@ -16,38 +21,68 @@ export class KuduServiceClient {
         this._webClient = new WebClient();
     }
 
-    public async beginRequest(request: WebRequest, reqOptions?: WebRequestOptions, contentType?: string): Promise<WebResponse> {
+    public async beginRequest(
+        request: WebRequest,
+        reqOptions?: WebRequestOptions,
+        contentType?: string
+    ): Promise<WebResponse> {
         request.headers = request.headers || {};
         request.headers["Authorization"] = this._accessToken;
-        request.headers['Content-Type'] = contentType || 'application/json; charset=utf-8';
+        request.headers["Content-Type"] =
+            contentType || "application/json; charset=utf-8";
 
-        if(!!this._cookie) {
-            core.debug(`setting affinity cookie ${JSON.stringify(this._cookie)}`);
-            request.headers['Cookie'] = this._cookie;
+        if (!!this._cookie) {
+            core.debug(
+                `setting affinity cookie ${JSON.stringify(this._cookie)}`
+            );
+            request.headers["Cookie"] = this._cookie;
         }
 
-        let retryCount = reqOptions && util.isNumber(reqOptions.retryCount) ? reqOptions.retryCount : 5;
+        let retryCount =
+            reqOptions && util.isNumber(reqOptions.retryCount)
+                ? reqOptions.retryCount
+                : 5;
 
-        while(retryCount >= 0) {
+        while (retryCount >= 0) {
             try {
-                let httpResponse = await this._webClient.sendRequest(request, reqOptions);
-                if(httpResponse.headers['set-cookie'] && !this._cookie) {
-                    this._cookie = httpResponse.headers['set-cookie'];
-                    core.debug(`loaded affinity cookie ${JSON.stringify(this._cookie)}`);
+                let httpResponse = await this._webClient.sendRequest(
+                    request,
+                    reqOptions
+                );
+                if (httpResponse.headers["set-cookie"] && !this._cookie) {
+                    this._cookie = httpResponse.headers["set-cookie"];
+                    core.debug(
+                        `loaded affinity cookie ${JSON.stringify(this._cookie)}`
+                    );
                 }
 
                 return httpResponse;
-            }
-            catch(exception) {
+            } catch (exception) {
                 let exceptionString: string = exception.toString();
-                if(exceptionString.indexOf("Hostname/IP doesn't match certificates's altnames") != -1
-                    || exceptionString.indexOf("unable to verify the first certificate") != -1
-                    || exceptionString.indexOf("unable to get local issuer certificate") != -1) {
-                        core.warning('To use a certificate in App Service, the certificate must be signed by a trusted certificate authority. If your web app gives you certificate validation errors, you\'re probably using a self-signed certificate and to resolve them you need to export variable named ACTIONS_AZURE_REST_IGNORE_SSL_ERRORS to the value true.');
+                if (
+                    exceptionString.indexOf(
+                        "Hostname/IP doesn't match certificates's altnames"
+                    ) != -1 ||
+                    exceptionString.indexOf(
+                        "unable to verify the first certificate"
+                    ) != -1 ||
+                    exceptionString.indexOf(
+                        "unable to get local issuer certificate"
+                    ) != -1
+                ) {
+                    core.warning(
+                        "To use a certificate in App Service, the certificate must be signed by a trusted certificate authority. If your web app gives you certificate validation errors, you're probably using a self-signed certificate and to resolve them you need to export variable named ACTIONS_AZURE_REST_IGNORE_SSL_ERRORS to the value true."
+                    );
                 }
 
-                if(retryCount > 0 && exceptionString.indexOf('Request timeout') != -1 && (!reqOptions || reqOptions.retryRequestTimedout)) {
-                    core.debug('encountered request timedout issue in Kudu. Retrying again');
+                if (
+                    retryCount > 0 &&
+                    exceptionString.indexOf("Request timeout") != -1 &&
+                    (!reqOptions || reqOptions.retryRequestTimedout)
+                ) {
+                    core.debug(
+                        "encountered request timedout issue in Kudu. Retrying again"
+                    );
                     retryCount -= 1;
                     continue;
                 }
@@ -55,14 +90,13 @@ export class KuduServiceClient {
                 throw exception;
             }
         }
-
     }
 
     public getRequestUri(uriFormat: string, queryParameters?: Array<string>) {
         uriFormat = uriFormat[0] == "/" ? uriFormat : "/" + uriFormat;
 
-        if(queryParameters && queryParameters.length > 0) {
-            uriFormat = uriFormat + '?' + queryParameters.join('&');
+        if (queryParameters && queryParameters.length > 0) {
+            uriFormat = uriFormat + "?" + queryParameters.join("&");
         }
 
         return this._scmUri + uriFormat;
--- src/utils/funcignore.ts
+++ src/utils/funcignore.ts
@@ -1,19 +1,22 @@
-import { existsSync, readFileSync,  } from 'fs';
-import { resolve, normalize } from 'path';
-import * as glob from 'glob';
-import * as rimraf from 'rimraf';
-import * as ignore from 'ignore';
-import { Logger } from './logger';
+import { existsSync, readFileSync } from "fs";
+import { resolve, normalize } from "path";
+import { glob, globSync } from "glob";
+import * as rimraf from "rimraf";
+import * as ignore from "ignore";
+import { Logger } from "./logger";
 
 export class FuncIgnore {
     public static doesFuncignoreExist(working_dir: string): boolean {
-        const funcignorePath: string = resolve(working_dir, '.funcignore');
-        return existsSync(funcignorePath)
+        const funcignorePath: string = resolve(working_dir, ".funcignore");
+        return existsSync(funcignorePath);
     }
 
     public static readFuncignore(working_dir: string): ignore.Ignore {
-        const funcignorePath: string = resolve(working_dir, '.funcignore');
-        const rules: string[] = readFileSync(funcignorePath).toString().split('\n').filter(l => l.trim() !== '');
+        const funcignorePath: string = resolve(working_dir, ".funcignore");
+        const rules: string[] = readFileSync(funcignorePath)
+            .toString()
+            .split("\n")
+            .filter((l) => l.trim() !== "");
 
         try {
             // @ts-ignore
@@ -23,27 +26,37 @@ export class FuncIgnore {
         }
     }
 
-    public static removeFilesFromFuncIgnore(working_dir: string, ignoreParser: ignore.Ignore): void {
+    public static removeFilesFromFuncIgnore(
+        working_dir: string,
+        ignoreParser: ignore.Ignore
+    ): void {
         if (!ignoreParser) {
-            Logger.Warn(`The ignore parser is undefined. Nothing will be removed.`);
+            Logger.Warn(
+                `The ignore parser is undefined. Nothing will be removed.`
+            );
             return;
         }
 
-        const sanitizedWorkingDir: string = FuncIgnore.sanitizeWorkingDir(working_dir);
-        const allFiles: string[] = glob.sync(`${sanitizedWorkingDir}/**/*`, { dot: true });
-        allFiles.forEach(name => {
-            const filename = name.replace(`${sanitizedWorkingDir}/`, '');
+        const sanitizedWorkingDir: string =
+            FuncIgnore.sanitizeWorkingDir(working_dir);
+        const allFiles: string[] = globSync(`${sanitizedWorkingDir}/**/*`, {
+            dot: true,
+        });
+        allFiles.forEach((name) => {
+            const filename = name.replace(`${sanitizedWorkingDir}/`, "");
             if (ignoreParser.ignores(filename)) {
                 try {
                     rimraf.sync(name, { maxBusyTries: 1 });
                 } catch (error) {
-                    Logger.Warn(`Failed to remove ${filename} (file defined in .gitignore)`);
+                    Logger.Warn(
+                        `Failed to remove ${filename} (file defined in .gitignore)`
+                    );
                 }
             }
-        })
+        });
     }
 
     private static sanitizeWorkingDir(working_dir: string): string {
-        return normalize(resolve(working_dir)).replace(/\\/g, '/');
+        return normalize(resolve(working_dir)).replace(/\\/g, "/");
     }
 }
